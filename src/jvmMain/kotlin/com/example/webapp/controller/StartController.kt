233c096cf867b2f9a630b0401369bc5a55c8e555
vendelieu <vendelieu@gmail.com>
2024-07-09 19:48:25 +0300

    fix errors

--- src/jvmMain/kotlin/com/example/webapp/controller/StartController.kt
+++ src/jvmMain/kotlin/com/example/webapp/controller/StartController.kt
@@ -3,12 +3,13 @@ package com.example.webapp.controller
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.annotations.CommandHandler
 import eu.vendeli.tgbot.annotations.UpdateHandler
-import eu.vendeli.tgbot.api.message
-import eu.vendeli.tgbot.api.sendMessage
+import eu.vendeli.tgbot.api.message.message
+import eu.vendeli.tgbot.api.message.sendMessage
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.MessageUpdate
 import eu.vendeli.tgbot.types.internal.UpdateType
 import eu.vendeli.tgbot.types.keyboard.WebAppInfo
+import eu.vendeli.tgbot.utils.checkIsInitDataSafe
 
 class StartController {
     @CommandHandler(["/start"])
@@ -21,6 +22,7 @@ class StartController {
     @UpdateHandler([UpdateType.MESSAGE])
     suspend fun messageObserver(update: MessageUpdate, user: User, bot: TelegramBot) {
         update.message.webAppData?.also {
+            if (it.data?.checkIsInitDataSafe("TOKEN", "HASH") == false) return // data is unsafe
             sendMessage { "We've got information from webapp: ${it.data}" }.send(user, bot)
         }
     }
